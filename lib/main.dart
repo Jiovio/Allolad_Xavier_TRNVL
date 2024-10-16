8bdb7764f9b9b2ee05319bd71da0772d42b72923
Vijay Sundar <112871288+sucvijay@users.noreply.github.com>
2024-10-16 14:56:51 +0530

    chk

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Sqlite.db();
   await initLocalStorage();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
